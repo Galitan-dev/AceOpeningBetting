4a2a0b3dff310724ad24729c151a6dfdd3c1c2fa
Galitan-dev <galitan.dev@gmail.com>
2021-12-27 14:36:35 +0100

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ echo Refreshing code
 rm -fr /app
 mkdir -p /app
 cd /app
-git clone https://galitan-dev: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@github.com/Galitan-dev/ThePistCyclabe-Bot /app
+git clone https://galitan-dev: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@github.com/Galitan-dev/AceOpeningBetting /app
 
 echo Installing packages
 yarn --production
